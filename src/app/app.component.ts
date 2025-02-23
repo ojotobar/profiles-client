6a8ba38b5a9ce123e0ac8f4d94efd5d3a3c7a5b1
Toba Ojo <ojotobar@gmail.com>
2025-02-23 12:25:07 +0100

    Used viewport for expand the sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideNavComponent } from "./side-nav/side-nav.component";
 import { ToolBarComponent } from "./tool-bar/tool-bar.component";
@@ -18,6 +18,8 @@ import { AppService } from './app.service';
 })
 export class AppComponent implements OnInit {
 	title = 'pro-files';
+	public getScreenWidth: number = 0;
+  	public getScreenHeight: number = 0;
 	sidebarExpanded: boolean = true;
 	isSidebarClosed: boolean = true;
 
@@ -25,5 +27,15 @@ export class AppComponent implements OnInit {
 		this.appService.getIsSidebarOpened.subscribe(s => this.isSidebarClosed = !s);
 	}
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+		this.getScreenWidth = window.innerWidth;
+    	this.getScreenHeight = window.innerHeight;
+	}
+
+	@HostListener('window:resize', ['$event'])
+	onWindowResize() {
+		this.getScreenWidth = window.innerWidth;
+		this.getScreenHeight = window.innerHeight;
+		this.sidebarExpanded = this.getScreenWidth >= 768 && !this.isSidebarClosed;
+	}
 }
