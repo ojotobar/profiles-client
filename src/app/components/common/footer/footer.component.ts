59f1415e79d5179b0dc67a3acf30a23ba19706c6
Toba Ojo <ojotobar@gmail.com>
2025-06-08 23:43:57 +0100

    Defaults version to 'Dev Version' on local environments

--- src/app/components/common/footer/footer.component.ts
+++ src/app/components/common/footer/footer.component.ts
@@ -17,7 +17,7 @@ export class FooterComponent implements OnInit {
   ngOnInit(): void {
     this.versionService.version$.subscribe({
       next: (v) => this.version = v,
-      error: () => this.version = '',
+      error: () => this.version = 'Dev Version',
     });
   }
 }
